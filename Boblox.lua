26bfa572dbd7a89eb9f78b4f1849388c526d5f38
Useless_AI <147249096+randomusernames@users.noreply.github.com>
2023-12-01 11:07:22 +0700

    Update Boblox.lua

--- Boblox.lua
+++ Boblox.lua
@@ -1,4 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt"))()
 for i,v in next, game:GetService("Players").LocalPlayer.Character:GetDescendants() do
 if v:IsA("BasePart") and v.Name ~="HumanoidRootPart" then 
 game:GetService("RunService").Heartbeat:connect(function()
