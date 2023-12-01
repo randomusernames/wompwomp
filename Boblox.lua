2414ec248baef4c03fbdc36fbe4255f444859590
Useless_AI <147249096+randomusernames@users.noreply.github.com>
2023-12-01 18:38:26 +0700

    Update Boblox.lua

--- Boblox.lua
+++ Boblox.lua
@@ -1,9 +1,9 @@
 game.Players.LocalPlayer.Character["Pink Hair"].Handle.Mesh:Destroy()
 game.Players.LocalPlayer.Character["LavanderHair"].Handle.Mesh:Destroy()
 game.Players.LocalPlayer.Character["Robloxclassicred"].Handle.Mesh:Destroy()
-char["Robloxclassicred"].Handle.AccessoryWeld:Destroy()
-char["Pink Hair"].Handle.AccessoryWeld:Destroy() 
-char["LavanderHair"].Handle.AccessoryWeld:Destroy()
+game.Players.LocalPlayer.Character["Robloxclassicred"].Handle.AccessoryWeld:Destroy()
+game.Players.LocalPlayer.Character["Pink Hair"].Handle.AccessoryWeld:Destroy() 
+game.Players.LocalPlayer.Character["LavanderHair"].Handle.AccessoryWeld:Destroy()
 HumanDied = false
 local CountSCIFIMOVIELOL = 1
 function SCIFIMOVIELOL(Part0,Part1,Position,Angle)
