8b4a360a0ef2b28cc7ae8d3d08b8049c28bb47fc
Useless_AI <147249096+randomusernames@users.noreply.github.com>
2023-12-01 11:17:04 +0700

    Update Boblox.lua

--- Boblox.lua
+++ Boblox.lua
@@ -306,5 +306,4 @@ for _,v in next, DeadChar:GetChildren() do
 end
 
 if ANIMATIONHERE then ANIMATIONHERE.Parent = CloneChar end
-wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomusernames/wompwomp/main/goodcopbadcop.lua"))()
